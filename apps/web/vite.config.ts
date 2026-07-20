d47ba8c9cb2ab38f09fbe7bd98fde21d94d81b55
Alexander Terentiev <terentiev.grizzly@gmail.com>
2026-07-20 17:06:44 +0300

    ci: deploy web app to GitHub Pages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base:
+    mode === 'production'
+      ? '/cargo-load-planner/'
+      : '/',
+
   plugins: [react()],
-})
+}))
