46d0b6af158ea3154a0f8a3c21940d2287cd13fe
Alexander Terentiev <terentiev.grizzly@gmail.com>
2026-07-21 21:46:23 +0300

    refactor: move planner types to packing engine

--- packages/packing-engine/src/index.ts
+++ packages/packing-engine/src/index.ts
@@ -1,1 +1,10 @@
+export type {
+  CargoOrientation,
+  CargoPosition,
+  CargoSpace,
+  CargoSpaceType,
+  CargoTemplate,
+  PlacedCargo,
+} from './model/types'
+
 export const PACKING_ENGINE_VERSION = '0.1.0'
---
+++ packages/packing-engine/src/model/types.ts
@@ -0,0 +1,51 @@
+export type CargoSpaceType =
+  | 'truck'
+  | 'container'
+  | 'van'
+  | 'custom'
+
+export type CargoSpace = {
+  id: string
+  name: string
+  type: CargoSpaceType
+  lengthMm: number
+  widthMm: number
+  heightMm: number
+  maxWeightKg: number
+}
+
+export type CargoOrientation =
+  | 'XYZ'
+  | 'XZY'
+  | 'YXZ'
+  | 'YZX'
+  | 'ZXY'
+  | 'ZYX'
+
+export type CargoTemplate = {
+  id: string
+  sku: string
+  name: string
+  lengthMm: number
+  widthMm: number
+  heightMm: number
+  weightKg: number
+  color: string
+  canBeTilted: boolean
+  stackable: boolean
+  maxTopLoadKg: number | null
+}
+
+export type CargoPosition = {
+  xMm: number
+  yMm: number
+  zMm: number
+}
+
+export type PlacedCargo = {
+  id: string
+  templateId: string
+  position: CargoPosition
+  orientation: CargoOrientation
+  locked: boolean
+}
